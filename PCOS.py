744aeaa522aac57e51ef41dc2a1eba38ff065ebb
Spawitra <76878259+Spawitra@users.noreply.github.com>
2022-04-02 02:11:46 +0700

    Update PCOS.py

--- PCOS.py
+++ PCOS.py
@@ -1,1 +1,17 @@
 
+import streamlit as st
+
+
+
+st.sidebar.header('แบบประเมินความเสี่ยงโรคถุงน้ำรังไข่หลายใบ')
+st.sidebar.subheader('กรอกข้อมูล')
+st.sidebar.write('--------------------------------------------------------------------')
+
+
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
