e090f493ccaef9077fb7f77e222033d8dc709cbf
Spawitra <76878259+Spawitra@users.noreply.github.com>
2022-04-02 20:24:43 +0700

    Update PCOS.py

--- PCOS.py
+++ PCOS.py
@@ -21,95 +21,84 @@ left, right, = st.columns(2)
 
 left.write('แบบประเมินความเสี่ยงโรคถุงน้ำรังไข่หลายใบ')
 left.write('กรอกข้อมูล')
-form = left.form("template_form")
+
 
 
 
 # รับ User input feature  X 
 def user_input_features():
-  Age = form.slider('อายุเท่าไหร่',0,100,22)
-  Weight= form.slider('น้ำหนัก (Kg)เท่าไหร่',0,150,79)
-  Cycle = form.slider('ประจำเดือนมากี่วัน',0,31,7)
-  Cycle = form.slider('ประจำเดือนมากี่วัน',0,31,7)
-  CycleLength= form.slider('ระยะห่างของรอบเดือน ห่างกันกี่วัน',0,60,16)
-  HairGrowth = form.slider('ขนตามจุดต่างๆเพิ่มขึ้นหรือไม่ ',0,1,1)
-  SkinDarkening= form.slider('ผิวดำคล้ำตามข้อต่างๆหรือไม่',0,1,0)
-  Pimples= form.slider('สิวเพิ่มขึ้นหรือไม่',0,1,1)
-  Fastfood= form.slider('รับประทานอาหารที่มีไขมันสูง (Fastfood) ',0,1,0)
-  FollicleL= form.slider('หน้ามันรูขุมขนทางด้านซ้าย กว้างขึ้นหรือไม่',0,1,1)
-  FollicleR= form.slider('หน้ามันรูขุมขนทางด้านขวา กว้างขึ้นหรือไม่',0,1,1)
-  WeightGain= form.slider( 'ช่วงนี้น้ำหนักเพิ่มขึ้นหรือไม่',0,1,1)
   
-  
+  Age = left.slider('อายุเท่าไหร่',0,100,22)
   left.write('อายุ', Age,'ปี')
   left.write(' # --------------------------------------')
   
-  
+  Weight= left.slider('น้ำหนัก (Kg)เท่าไหร่',0,150,79)
   left.write('น้ำหนัก', Weight, 'กิโลกรัม')
-  left.write(' # --------------------------------------')
+  st.sidebar.write(' # --------------------------------------')
   
+  Cycle = st.sidebar.slider('ประจำเดือนมากี่วัน',0,31,7)
+  st.sidebar.write('รอบเดือนมา', Cycle, 'วัน')
+  st.sidebar.write(' # --------------------------------------')
   
-  left.write('รอบเดือนมา', Cycle, 'วัน')
-  left.write(' # --------------------------------------')
+  CycleLength= st.sidebar.slider('ระยะห่างของรอบเดือน ห่างกันกี่วัน',0,60,16)
+  st.sidebar.write('ระยะห่างของรอบเดือน', CycleLength, 'วัน')
+  st.sidebar.write(' # --------------------------------------')
+  st.sidebar.write('### สอบถามอาการ ใช่(1)หรือไม่(0) ')
+  hairGrowth = st.sidebar.slider('ขนตามจุดต่างๆเพิ่มขึ้นหรือไม่ ',0,1,1)
+  st.sidebar.caption('''สังเกตตามจุดต่างๆบนร่างกาย ว่ามีขนเพิ่มขึ้นหรือไม่ เช่น จากไม่มีขนเลย เพิ่มขึ้นไประดับ1 หรือ มีขนที่ระดับ3แล้วเพิ่มขึ้นไประดับ4 ''')
+  st.sidebar.write('ขนเพิ่มขึ้นหรือไม่', hairGrowth)
+  st.sidebar.image(HairG, use_column_width = True)
+  st.sidebar.write(' # --------------------------------------')
   
-  
-  left.write('ระยะห่างของรอบเดือน', CycleLength, 'วัน')
-  left.write(' # --------------------------------------')
-  left.write('### สอบถามอาการ ใช่(1)หรือไม่(0) ')
-  
-  left.caption('''สังเกตตามจุดต่างๆบนร่างกาย ว่ามีขนเพิ่มขึ้นหรือไม่ เช่น จากไม่มีขนเลย เพิ่มขึ้นไประดับ1 หรือ มีขนที่ระดับ3แล้วเพิ่มขึ้นไประดับ4 ''')
-  left.write('ขนเพิ่มขึ้นหรือไม่', hairGrowth)
-  left.image(HairG, use_column_width = True)
-  left.write(' # --------------------------------------')
- 
-  
-  left.caption('ผิวคล้ำดำหนา ตามจุด ข้อนิ้ว ข้อศอก คอ หรือ รักแร้ เป็นต้น')
-  left.image(Skindarken, use_column_width = True)
-  left.write('ผิวดำคล้ำตามข้อต่างๆ', SkinDarkening)
-  left.write(' # --------------------------------------')
-  
-
-  
-  left.caption('สังเกตตนเองหากปกติไม่มีสิว แล้วสิวเกิดขึ้นมาเกินไป หากมีสิวขึ้นเยอะอยู่แล้วไม่ได้เพิ่มขึ้นถือว่าปกติ')
-  left.write('สิวเกิดเพิ่มขึ้น', Pimples)
-  left.write(' # --------------------------------------')
+  SkinDarkening= st.sidebar.slider('ผิวดำคล้ำตามข้อต่างๆหรือไม่',0,1,0)
+  st.sidebar.caption('ผิวคล้ำดำหนา ตามจุด ข้อนิ้ว ข้อศอก คอ หรือ รักแร้ เป็นต้น')
+  st.sidebar.image(Skindarken, use_column_width = True)
+  st.sidebar.write('ผิวดำคล้ำตามข้อต่างๆ', SkinDarkening)
+  st.sidebar.write(' # --------------------------------------')
   
 
-  
-  left.write('ทานอาหารที่มีไขมันสูงหรือไม่', Fastfood)
-  left.caption('ชอบรับประทานอาหารที่มีไขมันสูง หรือ ทานบ่อยครั้ง')
-  left.write(' # --------------------------------------')
-  
-
-  
-  left.write('หน้ามันและรูขุมขนกว้างทางด้านซ้ายหรือไม่', FollicleL)
-  left.write(' # --------------------------------------')
+  Pimples= st.sidebar.slider('สิวเพิ่มขึ้นหรือไม่',0,1,1)
+  st.sidebar.caption('สังเกตตนเองหากปกติไม่มีสิว แล้วสิวเกิดขึ้นมาเกินไป หากมีสิวขึ้นเยอะอยู่แล้วไม่ได้เพิ่มขึ้นถือว่าปกติ')
+  st.sidebar.write('สิวเกิดเพิ่มขึ้น', Pimples)
+  st.sidebar.write(' # --------------------------------------')
   
 
-  
-  left.write('หน้ามันและรูขุมขนกว้างทางด้านขวาหรือไม่' , FollicleR)
-  left.write(' # --------------------------------------')
+  Fastfood= st.sidebar.slider('รับประทานอาหารที่มีไขมันสูง (Fastfood) ',0,1,0)
+  st.sidebar.write('ทานอาหารที่มีไขมันสูงหรือไม่', Fastfood)
+  st.sidebar.caption('ชอบรับประทานอาหารที่มีไขมันสูง หรือ ทานบ่อยครั้ง')
+  st.sidebar.write(' # --------------------------------------')
   
 
+  FollicleL= st.sidebar.slider('หน้ามันรูขุมขนทางด้านซ้าย กว้างขึ้นหรือไม่',0,1,1)
+  st.sidebar.write('หน้ามันและรูขุมขนกว้างทางด้านซ้ายหรือไม่', FollicleL)
+  st.sidebar.write(' # --------------------------------------')
   
-  left.caption('น้ำหนักเพิ่มขึ้นแบบรวดเร็วหรือไม่ เช่น จาก60เพิ่มไป 70 ในระยะเวลาสั้นๆ')
-  left.write('น้ำหนักเพิ่มขึ้น', WeightGain)
-  left.write(' # --------------------------------------')
+
+  FollicleR= st.sidebar.slider('หน้ามันรูขุมขนทางด้านขวา กว้างขึ้นหรือไม่',0,1,1)
+  st.sidebar.write('หน้ามันและรูขุมขนกว้างทางด้านขวาหรือไม่' , FollicleR)
+  st.sidebar.write(' # --------------------------------------')
+  
+
+  WeightGain= st.sidebar.slider( 'ช่วงนี้น้ำหนักเพิ่มขึ้นหรือไม่',0,1,1)
+  st.sidebar.caption('น้ำหนักเพิ่มขึ้นแบบรวดเร็วหรือไม่ เช่น จาก60เพิ่มไป 70 ในระยะเวลาสั้นๆ')
+  st.sidebar.write('น้ำหนักเพิ่มขึ้น', WeightGain)
+  st.sidebar.write(' # --------------------------------------')
+  
   
   pipe =  { 'Age (yrs)': Age,
-           'Weight (Kg)': Weight,
-           'Cycle(R/I)': Cycle,
-           'Cycle length(days)': CycleLength,
-           'hair growth(Y/N)': HairGrowth,
-           'Skin darkening (Y/N)': SkinDarkening,
-           'Pimples(Y/N)': Pimples,
-           'Fast food (Y/N)': Fastfood,
-           'Follicle No. (L)': FollicleL,
-           'Follicle No. (R)': FollicleR,
-           'Weight gain(Y/N)': WeightGain}
+             'Weight (Kg)': Weight, 
+             'Cycle(R/I)': Cycle,
+              'Cycle length(days)': CycleLength,
+              'hair growth(Y/N)': hairGrowth, 
+               'Skin darkening (Y/N)': SkinDarkening,
+               'Pimples(Y/N)': Pimples,
+              'Fast food (Y/N)': Fastfood,
+               'Follicle No. (L)': FollicleL, 
+                'Follicle No. (R)': FollicleR, 
+                'Weight gain(Y/N)': WeightGain}
+
   features = pd.DataFrame(pipe, index=[0])
   return features
-submit = form.form_submit_button("ประเมินความเสี่ยง")
 
 app  = load('PcosApp.joblib')
 
